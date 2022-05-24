386f3aa217955ae2ae4f027bed942fe4175672da
Francis Osei <oseifrancis673@gmail.com>
2022-05-24 16:29:19 +0100

    add ux to modal

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,64 +1,17 @@
-import icons from "../img/icons.svg";
-
-console.log(`${icons}`);
-
 const ListContainer = document.getElementById("todo__list");
 const addTaskBtn = document.getElementById("add_to_list");
+const openModal = document.getElementById("openModal");
+const btnCloseModal = document.getElementById("btnClose");
+const modalPopup = document.getElementById("popup");
 const c = document.querySelector(".todo__c");
 let n = 0;
 let taskList = [];
 
-// popup.addEventListener("keypress", (e) => {
-//   if (e.key === "Escape") renderTask();
-// });
-
-if (ListContainer) {
-  ListContainer.addEventListener(
-    "click",
-    (e) => {
-      const removeList = e.target.parentNode.closest("#remove-task");
-
-      if (!removeList) return;
-      removeTask(removeList);
-
-      // switch (e.target.tagName) {
-      //   case "svg":
-      //     break;
-      // }
-    },
-    true
-  );
-}
-if (ListContainer) {
-  ListContainer.addEventListener("change", (e) => {
-    if (e.target.tagName === "INPUT" && e.target.type === "checkbox") {
-      completeTodo(e.target);
-    }
-  });
-}
-
-if (addTaskBtn) {
-  addTaskBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const title = document.getElementById("titleInput").value.trim();
-    const description = document
-      .getElementById("descriptionInput")
-      .value.trim();
-    clearInput();
-    if (title.length === 0 || description.length === 0) return;
-    renderTask(title, description);
-    clearInput();
-  });
-}
-
-const clearInput = () => {
-  document.getElementById("titleInput").value = "";
-  document.getElementById("descriptionInput").value = "";
-};
-
 ////////////////////////////////////////////////
 // FUNCTION
+const closeModal = () => {
+  modalPopup.classList.add("hidden");
+};
 
 const completeTodo = (inputElement) => {
   inputElement.nextElementSibling.classList.toggle("complete");
@@ -111,4 +64,71 @@ const renderTask = (title, description) => {
   c.style.display = "none";
   ListContainer.insertAdjacentHTML("beforeend", markup);
   taskList.push({ listItemId: n, title, description });
+};
+
+// popup.addEventListener("keypress", (e) => {
+//   if (e.key === "Escape") renderTask();
+// });
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modalPopup.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+modalPopup.addEventListener("click", (e) => {
+  if (e.target.className === "popup") {
+    closeModal();
+  }
+});
+btnCloseModal.addEventListener("click", closeModal);
+
+openModal.addEventListener("click", (e) => {
+  modalPopup.classList.remove("hidden");
+});
+
+if (ListContainer) {
+  ListContainer.addEventListener(
+    "click",
+    (e) => {
+      const removeList = e.target.parentNode.closest("#remove-task");
+
+      if (!removeList) return;
+      removeTask(removeList);
+
+      // switch (e.target.tagName) {
+      //   case "svg":
+      //     break;
+      // }
+    },
+    true
+  );
+}
+if (ListContainer) {
+  ListContainer.addEventListener("change", (e) => {
+    if (e.target.tagName === "INPUT" && e.target.type === "checkbox") {
+      completeTodo(e.target);
+    }
+  });
+}
+
+if (addTaskBtn) {
+  addTaskBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("titleInput").value.trim();
+    const description = document
+      .getElementById("descriptionInput")
+      .value.trim();
+    clearInput();
+    if (title.length === 0 || description.length === 0) return;
+    renderTask(title, description);
+    clearInput();
+    closeModal();
+  });
+}
+
+const clearInput = () => {
+  document.getElementById("titleInput").value = "";
+  document.getElementById("descriptionInput").value = "";
 };
